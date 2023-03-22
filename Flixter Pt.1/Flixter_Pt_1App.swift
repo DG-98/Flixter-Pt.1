09041f755a37c0eece8202d283ce32fad803284f
Derrick Garcon <dgarconpro@gmail.com>
2023-03-22 14:29:30 -0400

    Initial Commit

---
+++ Flixter Pt.1/Flixter_Pt_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Flixter_Pt_1App.swift
+//  Flixter Pt.1
+//
+//  Created by Derrick on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Flixter_Pt_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
